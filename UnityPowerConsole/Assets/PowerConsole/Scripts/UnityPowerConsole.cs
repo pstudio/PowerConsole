958abb9f9a5703ca0420fd944261b9320959c8f7
Peter Pedersen <pstudio.tk@gmail.com>
2017-03-20 21:50:40 +0100

    Added the Unity project

---
+++ UnityPowerConsole/Assets/PowerConsole/Scripts/UnityPowerConsole.cs
@@ -0,0 +1,99 @@
+﻿
+using System.Linq;
+using System.Text;
+using JetBrains.Annotations;
+using pstudio.PowerConsole;
+using pstudio.PowerConsole.Host;
+using TMPro;
+using UnityEngine;
+
+
+public class UnityPowerConsole : MonoBehaviour, IHost
+{
+
+    [SerializeField] private TMP_InputField _input;
+    [SerializeField] private TMP_InputField _output;
+
+    private PowerConsole _console;
+
+
+    [UsedImplicitly]
+    private void Awake()
+    {
+        _console = new PowerConsole(this);
+    }
+
+    [UsedImplicitly]
+    private void OnEnable()
+    {
+        _input.onSubmit.AddListener(SubmitCommand);
+    }
+
+    [UsedImplicitly]
+    private void OnDisable()
+    {
+        _input.onSubmit.RemoveListener(SubmitCommand);
+    }
+
+    private void SubmitCommand(string command)
+    {
+        _input.text = string.Empty;
+
+        OutputText(command, OutputColorType.Default);
+
+
+        _console.Execute(command);
+
+        //var history = _console.GetHistory();
+        //StringBuilder sb = new StringBuilder();
+        //foreach (var s in history)
+        //{
+        //    sb.AppendLine(s);
+        //}
+        //sb.AppendLine(reply);
+        //_output.text += "\n" + command;
+
+        //_output.text = sb.ToString();
+        //_output.verticalScrollbar.value = 1;
+
+        _input.ActivateInputField();
+    }
+
+    private static readonly string[] ColorPalette =
+    {
+        "#F0F0F0",
+        "#F0F020",
+        "#AAAAAA",
+        "#FF2020",
+        "#FFAAAA",
+        "#AAAAFF",
+    };
+
+    public string FormatColor(string message, OutputColorType colorType)
+    {
+        return "<color=" + ColorPalette[(int) colorType] + ">" + message + "</color>";
+    }
+
+    private void OutputText(string message, OutputColorType colorType)
+    {
+        _output.text += FormatColor(message + "\n", colorType);
+        //_output.verticalScrollbar.value = 1;
+    }
+
+    public void Write(string message)
+    {
+        OutputText("~ " + message, OutputColorType.Default);
+    }
+
+    public void WriteDebug(string debugMessage)
+    {
+        OutputText("[DEBUG]~ " + debugMessage, OutputColorType.Debug);
+    }
+
+    public void WriteError(string errorMessage)
+    {
+        OutputText("[ERROR]~ " + errorMessage, OutputColorType.Error);
+    }
+
+    public bool SupportsColor { get { return true; } }
+}
